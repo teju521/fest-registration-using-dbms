fdef7f945eb2c12789eb1e3aa328f2662392f125
Teja sri <teja.bitra@gmail.com>
2016-10-16 17:34:22 +0530

    Update striker.php

--- striker.php
+++ striker.php
@@ -1,5 +1,5 @@
 <?php
-$s=new HTTP_Request2('http://www.smsstriker.com/API/sms.php?username=vvit999&password=vvssbpmsk&from=VVITGT&to=9573575607&msg=hi testing&type=1',HTTP_Request2::METH_GET);
+$s=new HTTP_Request2('http://www.smsstriker.com/API/sms.php?username=<username>&password=<password>&from=VVITGT&to=9xxxxxxxx&msg=hi testing&type=1',HTTP_Request2::METH_GET);
 try{
 	$s->send();
 	if($s->getResponseCode()==200)
